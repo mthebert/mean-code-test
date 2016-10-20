a20e456d2520aeb70f2a55dde10931374b774f97
Matthew Thebert <matt@thebert.com>
2016-10-20 04:28:17 -0400

    dropdown directive, split controller into own file

---
+++ public/app/controllers/mainController.js
@@ -0,0 +1,34 @@
+angular.module('mainCtrl',[])
+.controller('mainController', function($http) {
+  var vm = this;
+  vm.message = 'Pick a Car Make and Model Above';
+
+  $http.get('/api/makes')
+    .then(function(data) {
+      vm.makes = data.data.makes;
+    });
+
+  $http.get('/api/models/5')
+    .then(function(data) {
+      // vm.carmodel = data.data;
+    });
+})
+
+.directive('dropdown', function($http) {
+  return {
+    restrict: 'E',
+    scope: {
+      makes: "="
+    },
+    templateUrl: 'app/views/pages/dropdown.html',
+    link: function(scope) {
+      scope.selectMake = function(id) {
+        console.log("selected " + id)
+      }
+      $http.get('/api/models')
+        .then(function(data) {
+          scope.models = data.data.models;
+        });
+    }
+  };
+})
